1ed26248f64df694036ada9987d8bbd9da5f4838
ADITYA_MISHRA <90920322+Adityamishra45@users.noreply.github.com>
2023-06-03 17:25:15 +0530

    Add files via upload

---
+++ Day1/Literals.java
@@ -0,0 +1,10 @@
+public class Literals   // Literals:- A constant value which can be assigned to any variable.
+{
+    public static void main(String[] args) {
+        byte age = 20;      // Integer literal
+        char ch = 'A';      // charecter literal
+        float f1 = 5.6f;    //double literal :- default
+        double d1 = 5.66;   //Double literal
+        System.out.println(age);
+    }
+}
